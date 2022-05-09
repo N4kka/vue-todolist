12984dc30bacb43e38c2bb7b76ad48cba801b7d6
Nicola Sarli <you@example.com>
2022-05-09 16:36:42 +0200

    print text in HTML

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-alert("ciao");
-
 const app = new Vue (
     {
-        el: '#root'
+        el: '#root',
+        data: {
+            toDoTasks: [
+                {
+                    text: "prendere un caffè",
+                    done: false
+                },
+                {
+                    text: "iniziare l'esercizio",
+                    done: true
+                },
+                {
+                    text: "consegnare l'esercizio",
+                    done: false
+                },
+                {
+                    text: "andare in palestra",
+                    done: false
+                }
+            ]
+        },
+        methods: {
+        
+        }
     }
 )
